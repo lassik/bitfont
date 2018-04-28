98447eaa6e432d5487b4efb5c259fcc9ed88beab
Lassi Kortela <lassi@lassi.io>
2018-04-28 22:24:35 +0300

    Fix typo in conversions/scanlines.sh

--- conversions/scanlines.sh
+++ conversions/scanlines.sh
@@ -11,6 +11,6 @@ shasum scanlines.zip | diff -u scanlines.sha1 -
 mkdir -p ../dumbfont16
 rm -rf work
 mkdir work
-d work && unzip ../scanlines.zip)
+(cd work && unzip ../scanlines.zip)
 ttf2dumbfont work/Scanlines.ttf 16 >../dumbfont16/scanlines.dumbfont
 cat work/readme.txt work/license.txt >../dumbfont16/scanlines.license.text

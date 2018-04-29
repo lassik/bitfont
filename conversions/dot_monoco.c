de60d9cc5614177ae177bc54b6c7a86a82f0191e
Lassi Kortela <lassi@lassi.io>
2018-04-29 12:07:56 +0300

    Reformat

--- conversions/dot_monoco.c
+++ conversions/dot_monoco.c
@@ -12,42 +12,46 @@ static const unsigned char unisig[32] =
 static unsigned char pixels[128 * 32];
 static unsigned int ascii2offset[128];
 
-static unsigned int reverse_byte(unsigned int byte) {
-  unsigned int etyb, i;
+static unsigned int
+reverse_byte(unsigned int byte)
+{
+    unsigned int etyb, i;
 
-  etyb = 0;
-  for (i = 0; i < 8; i++) {
-    etyb <<= 1;
-    etyb |= byte & 1;
-    byte >>= 1;
-  }
-  return etyb;
+    etyb = 0;
+    for (i = 0; i < 8; i++) {
+        etyb <<= 1;
+        etyb |= byte & 1;
+        byte >>= 1;
+    }
+    return etyb;
 }
 
-int main(void) {
-  unsigned char *bytep;
-  unsigned int ascii, offset, i;
+int
+main(void)
+{
+    unsigned char *bytep;
+    unsigned int ascii, offset, i;
 
-  offset = 0;
-  while (offset < sizeof(chars)) {
-    ascii = chars[offset++];
-    ascii2offset[ascii] = offset;
-    offset += 32;
-  }
-  bytep = pixels;
-  for (ascii = 0; ascii < 128; ascii++) {
-    if ((offset = ascii2offset[ascii])) {
-      for (i = 0; i < 32; i++) {
-        *bytep++ = reverse_byte(~chars[offset++]);
-      }
-    } else {
-      bytep += 32;
+    offset = 0;
+    while (offset < sizeof(chars)) {
+        ascii = chars[offset++];
+        ascii2offset[ascii] = offset;
+        offset += 32;
     }
-  }
-  if ((1 != fwrite(unisig, sizeof(unisig), 1, stdout)) ||
-      (1 != fwrite(pixels, sizeof(pixels), 1, stdout))) {
-    fprintf(stderr, "fail\n");
-    exit(1);
-  }
-  return 0;
+    bytep = pixels;
+    for (ascii = 0; ascii < 128; ascii++) {
+        if ((offset = ascii2offset[ascii])) {
+            for (i = 0; i < 32; i++) {
+                *bytep++ = reverse_byte(~chars[offset++]);
+            }
+        } else {
+            bytep += 32;
+        }
+    }
+    if ((1 != fwrite(unisig, sizeof(unisig), 1, stdout)) ||
+        (1 != fwrite(pixels, sizeof(pixels), 1, stdout))) {
+        fprintf(stderr, "fail\n");
+        exit(1);
+    }
+    return 0;
 }
